43bb3ce8b8c2f9866c1dca3cdd8f0b344c168ec4
Avetik <xavetikx@gmail.com>
2024-11-15 10:11:03 +0300

    C sharp events input system

--- Assets/PlayerInputController.cs
+++ Assets/PlayerInputController.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(PlayerInput))]
 public class PlayerInputController : MonoBehaviour
 {
+    private PlayerInput _playerInput;
+
+    private void Awake()
+    {
+        _playerInput = new PlayerInput();
+
+        _playerInput.Player.Shoot.performed += OnShoot;
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+
     public void OnShoot(InputAction.CallbackContext context)
     {
         Debug.Log("Shoot");
